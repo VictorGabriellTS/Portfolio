cd19cde536252da54625d66b77429d04587da875
Victor Gabriel <fortxoto13@gmail.com>
2023-12-09 02:51:18 -0300

    add-footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.scss";
+
+function Footer() {
+    return (
+        <>
+            <footer className="footer">
+                <p>Copywrite & Copy : 2023 | All Right Reserved by VictorGTS</p>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
